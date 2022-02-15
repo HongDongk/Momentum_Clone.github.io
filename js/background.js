41df02729f5eef8075843a11641781382ab418f3
Dongkeun <64244383+HongDongk@users.noreply.github.com>
2022-02-15 16:25:35 +0900

    background_update

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img"); //html에 img라는 element 생성
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage); //html에 img라는 element 추가 
