4c30a75ccdf4cbec7b04562d3bb7c5c124fc968d
Dongkeun <64244383+HongDongk@users.noreply.github.com>
2022-02-16 16:53:27 +0900

    weather_update

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "6d79a5e04af764eae57ead6117426259";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+  console.log(  lat, lon );
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); //위치정보를 받아오게해줌
